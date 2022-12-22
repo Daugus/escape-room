26042316dfe84d531fcc4633264f0fb396867bb1
Optygd <carmengd2003@gmail.com>
2022-12-22 12:33:24 +0100

    seeder y factory de usuario

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'role' => $this->faker->randomElement(['profesor', 'alumno']),
+            'name' => $this->faker->firstName(),
+            'surname' => $this->faker->lastName(),
+            'nickname' => $this->faker->unique()->word(),
+            'password' => $this->faker->password(),
+            'email' => $this->faker->email(),
+            'picture' => 'ejemplo.png'
+        ];
+    }
+}
