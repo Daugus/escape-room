d7b1c0c24a46dd190a8691e48ad20a3b1cdff800
Augusto <augustodelacamara@gmail.com>
2023-01-13 12:54:37 +0100

    juntado controllers de hangman

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HangmanSolutionController;
+use App\Http\Controllers\HangmanController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
 // Ruta para coger el JSON de Hangman desde VUE
-// Route::post('hangman/getWord', 'HangmanSolutionController@getWord')->name('api.hangman.getWord');
-Route::post('hangman/getWord', [HangmanSolutionController::class, 'getWord']);
+Route::post('hangman/getWord', [HangmanController::class, 'getWord']);
