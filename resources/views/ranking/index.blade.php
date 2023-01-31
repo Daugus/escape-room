2668fdfe7349dd453e0d25d65f42051e45d13992
daugus <augustodelacamara@gmail.com>
2023-01-31 23:25:03 +0100

    arreglado ancho de tabla

--- resources/views/ranking/index.blade.php
+++ resources/views/ranking/index.blade.php
@@ -44,15 +44,15 @@
         <table class="table-auto">
             <thead>
                 <tr>
-                    <th
-                        class="border border-gray-700 bg-neutral-200 appearance-none w-full text-gray-700 rounded py-3 px-4">
-                        Tiempo</th>
-                    <th
-                        class="border border-gray-700 bg-neutral-200 appearance-none w-full text-gray-700 rounded py-3 px-4">
-                        Dificultad</th>
-                    <th
-                        class="border border-gray-700 bg-neutral-200 appearance-none w-full text-gray-700 rounded py-3 px-4">
-                        Grupo</th>
+                    <th class="border border-gray-700 bg-neutral-200 appearance-none text-gray-700 rounded py-3 px-4">
+                        Tiempo
+                    </th>
+                    <th class="border border-gray-700 bg-neutral-200 appearance-none text-gray-700 rounded py-3 px-4">
+                        Dificultad
+                    </th>
+                    <th class="border border-gray-700 bg-neutral-200 appearance-none text-gray-700 rounded py-3 px-4">
+                        Grupo
+                    </th>
                 </tr>
             </thead>
 
