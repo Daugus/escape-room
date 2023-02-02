61f69189c55ee5ee03808eda2154df3dedf8544f
lukaquindon08 <lukacarmona115@gmail.com>
2023-02-02 09:43:08 +0100

    funcionalidad contraseña a medias

---
+++ resources/js/components/password-needed/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import Button from "./Button.vue";
+</script>
+
+<script>
+export default {
+    data() {
+        return {
+            letras: [
+                "A",
+                "B",
+                "C",
+                "D",
+                "E",
+                "F",
+                "G",
+                "H",
+                "I",
+                "J",
+                "K",
+                "L",
+                "M",
+                "N",
+                "Ñ",
+                "O",
+                "P",
+                "Q",
+                "R",
+                "S",
+                "T",
+                "U",
+                "V",
+                "W",
+                "X",
+                "Y",
+                "Z",
+            ],
+            numeros: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+        };
+    },
+};
+</script>
+
+<template>
+    <Button :caracteres="letras"></Button>
+    <Button :caracteres="numeros"></Button>
+    <Button :caracteres="numeros"></Button>
+    <Button :caracteres="numeros"></Button>
+    <Button :caracteres="numeros"></Button>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ resources/js/components/password-needed/Button.vue
@@ -0,0 +1,33 @@
+<script setup>
+import route from "ziggy";
+</script>
+
+<script>
+export default {
+    props: {
+        caracteres: Array,
+        i: 0,
+    },
+    computed: {
+        sumar() {
+            this.i = this.i++;
+        },
+        restar() {
+            this.i = this.i--;
+        },
+        rutaHref() {
+            return route(this.ruta);
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="grid grid-cols-1 grid-rows-3">
+        <button @click="sumar"></button>
+        <p>{{ caracteres[i] }}</p>
+        <button @click="restar"></button>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
